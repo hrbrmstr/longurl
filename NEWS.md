e77e9b5e4653cc12ad5e189d2ff14419244c2482
Bob Rudis <bob@rud.is>
2016-12-17 13:39:03 -0500

    Removed dependency on API services

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# longurl 0.3.0
+
+* No longer uses an API service
+* Returned `tbl_df` has an extra status code field
+
 # longurl 0.2.0
 
 * Switched out the API service since longurl.org is no longer working
--- R/longurl-package.r
+++ R/longurl-package.r
@@ -1,11 +1,8 @@
-#' A package to work with the URL-Expander API
-#'
-#' Interface to the 'URL-Expander' API to expand vectors of short URLs
-#' with optional error checking and URL validation.
+#' Tools expand vectors of short URLs into long URLs
 #'
 #' @name longurl
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
-#' @import httr dplyr pbapply
-#' @importFrom jsonlite fromJSON
+#' @import httr dplyr
+#' @importFrom purrr safely map map_df map_chr map_lgl
 NULL
